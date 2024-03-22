991488958737c13b6837ba9418f5b8cbb3aeeb0a
francisco lopez <142629221+francilopez@users.noreply.github.com>
2024-03-22 15:34:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,3 +10,4 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide1').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
